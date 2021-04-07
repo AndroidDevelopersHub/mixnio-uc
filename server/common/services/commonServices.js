14b0d888790744e52f4973c89ce9357d47df82c6
Tonmoy ParallaxLogic <tomnoy@parallaxlogic.net>
2021-04-08 00:13:54 +0600

    test

--- server/common/services/commonServices.js
+++ server/common/services/commonServices.js
@@ -2,12 +2,15 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 function salt(pass){
+    var response = "test"
     bcrypt.genSalt(saltRounds, function(err, salt) {
         bcrypt.hash(pass, salt, function(err, hash) {
             // Store hash in your password DB.
+            response = hash
             return hash
         });
     });
+    return response
 }
 
 
