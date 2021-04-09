b14e7fdde6326966ec561b819d1a2918b73828dc
Tonmoy ParallaxLogic <tomnoy@parallaxlogic.net>
2021-04-09 16:14:13 +0600

    user add api

--- server/api/db.js
+++ server/api/db.js
@@ -1,21 +1,21 @@
 const mysql = require("mysql");
 
 // Dev
-/*const connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     user: "mixcweng_api",
     password: "1982gonzoO",
     database: "mixcweng_api"
-});*/
+});
 
 //Local
-const connection = mysql.createConnection({
+/*const connection = mysql.
     host: "localhost",
     user: "root",
     password: "123456", // "" , "root"
     database: "mixcweng_api",
     port: 8889
-});
+});*/
 
 
 connection.connect((err) => {
