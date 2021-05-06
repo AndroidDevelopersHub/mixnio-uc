204f2f7eb17e24f07876d4be06c57440afd76703
Tonmoy ParallaxLogic <tomnoy@parallaxlogic.net>
2021-05-06 23:20:40 +0600

    update

--- server/api/game_pass.js
+++ server/api/game_pass.js
@@ -21,6 +21,9 @@ module.exports = function (router) {
     router.get('/game_pass/:id', details);
     router.delete('/game_pass/:id', _delete);
 
+    router.post('/game_pass/join', join);
+    router.get('/game_pass/join_list', joinList);
+
 }
 
 
@@ -31,6 +34,77 @@ const schema = Joi.object({
     salt: Joi.string().required(),
     //token: Joi.string().required()
 });
+
+
+function join(req, res){
+    //
+    // const { error } = schema.validate(req.body);
+    // if (error) return _response.apiFailed(res ,error.details[0].message)
+
+    console.log('User not exist')
+    db.query("INSERT INTO game_pass_entry SET ?", req.body , (err, result) => {
+        if (!err) {
+            return _response.apiSuccess(res, responsemsg.saveSuccess , result)
+        } else {
+            return _response.apiFailed(res, err , result)
+        }
+    });
+}
+
+
+
+async function joinList(req ,res ){
+
+    var limit = 500;
+    var page = 1;
+    var totalDocs = 0;
+    if (req.query.page){
+        page = req.query.page
+    }
+    if (req.query.limit){
+        limit = req.query.limit
+    }
+    var offset = (page - 1) * limit
+
+
+    db.query("SELECT COUNT(*) AS total FROM game_pass_entry", (err, result) => {
+        if (!err) {
+            totalDocs = result[0].total
+        } else {
+
+        }
+    });
+
+
+
+    //Search by String
+    if (req.query.search_string && req.query.search_string !== ''){
+
+        db.query("SELECT * FROM game_pass_entry WHERE CONCAT(uid,game_pass_id,winner) REGEXP '"+req.query.search_string+"'  LIMIT "+limit+" OFFSET "+offset+" ", (err, result) => {
+            if (!err && result.length > 0) {
+                return _response.apiSuccess(res, result.length+" "+responsemsg.found , result,{page: parseInt(page) , limit: parseInt(limit),totalDocs: totalDocs })
+
+            } else {
+                return _response.apiFailed(res, responsemsg.listIsEmpty)
+            }
+        });
+
+
+    }else {
+        db.query("SELECT * FROM game_pass_entry LIMIT "+limit+" OFFSET "+offset+" ", (err, result) => {
+            if (!err) {
+                return _response.apiSuccess(res, result.length+" "+responsemsg.found , result , {page: parseInt(page) , limit: parseInt(limit),totalDocs: totalDocs })
+
+            } else {
+                return _response.apiFailed(res, responsemsg.listIsEmpty )
+            }
+        });
+    }
+
+
+}
+
+
 
 function add(req, res){
     //
