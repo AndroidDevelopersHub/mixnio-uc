29f09db667f5be20bf0dd92c538022fb204e1679
Tonmoy ParallaxLogic <tomnoy@parallaxlogic.net>
2021-05-16 14:38:39 +0600

    update new

--- server/api/index.js
+++ server/api/index.js
@@ -11,6 +11,10 @@ require('./transaction')(router)
 require('./redeem')(router)
 require('./package')(router)
 require('./game_pass')(router)
+require('./news')(router)
+require('./payouts')(router)
+require('./payout_history')(router)
+require('./slider')(router)
 
 module.exports = router;
 
